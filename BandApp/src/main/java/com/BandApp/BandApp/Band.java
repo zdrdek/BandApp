98558df85cbb75a6ffa72e48e33dc53f1e1e3b14
Zack Drdek <zdrdek@gmail.com>
2018-07-17 10:09:05 -0400

    initial commit

---
+++ BandApp/src/main/java/com/BandApp/BandApp/Band.java
@@ -0,0 +1,118 @@
+package com.BandApp.BandApp;
+
+import java.awt.Image;
+
+import javax.sound.sampled.Clip;
+
+public class Band {
+	
+		private Long id;
+	    private String bandName;
+	    private String genreOne;
+	    private String genreTwo;
+	    private String genreThree;
+		private String influenceOne;
+	    private String influenceTwo;
+	    private String influenceThree;
+	    private String influenceFour;
+	    private String influenceFive;
+	    private String biography;
+	    private String bandPic;
+	    private String songOne;
+	    private String songTwo;
+	    private String songthree;
+	    
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getBandName() {
+			return bandName;
+		}
+		public void setBandName(String bandName) {
+			this.bandName = bandName;
+		}
+		public String getGenreOne() {
+			return genreOne;
+		}
+		public void setGenreOne(String genreOne) {
+			this.genreOne = genreOne;
+		}
+		public String getGenreTwo() {
+			return genreTwo;
+		}
+		public void setGenreTwo(String genreTwo) {
+			this.genreTwo = genreTwo;
+		}
+		public String getGenreThree() {
+			return genreThree;
+		}
+		public void setGenreThree(String genreThree) {
+			this.genreThree = genreThree;
+		}
+		public String getInfluenceOne() {
+			return influenceOne;
+		}
+		public void setInfluenceOne(String influenceOne) {
+			this.influenceOne = influenceOne;
+		}
+		public String getInfluenceTwo() {
+			return influenceTwo;
+		}
+		public void setInfluenceTwo(String influenceTwo) {
+			this.influenceTwo = influenceTwo;
+		}
+		public String getInfluenceThree() {
+			return influenceThree;
+		}
+		public void setInfluenceThree(String influenceThree) {
+			this.influenceThree = influenceThree;
+		}
+		public String getInfluenceFour() {
+			return influenceFour;
+		}
+		public void setInfluenceFour(String influenceFour) {
+			this.influenceFour = influenceFour;
+		}
+		public String getInfluenceFive() {
+			return influenceFive;
+		}
+		public void setInfluenceFive(String influenceFive) {
+			this.influenceFive = influenceFive;
+		}
+		
+		public String getBiography() {
+			return biography;
+		}
+		public void setBiography(String biography) {
+			this.biography = biography;
+		}
+		public String getBandPic() {
+			return bandPic;
+		}
+		public void setBandPic(String bandPic) {
+			this.bandPic = bandPic;
+		}
+		public String getSongOne() {
+			return songOne;
+		}
+		public void setSongOne(String songOne) {
+			this.songOne = songOne;
+		}
+		public String getSongTwo() {
+			return songTwo;
+		}
+		public void setSongTwo(String songTwo) {
+			this.songTwo = songTwo;
+		}
+		public String getSongthree() {
+			return songthree;
+		}
+		public void setSongthree(String songthree) {
+			this.songthree = songthree;
+		}
+	    
+
+}
